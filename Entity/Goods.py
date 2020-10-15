4709c55b87499ee8d519183d1e3650c04096e6a7
Ray <deity6686@gmail.com>
2020-10-15 11:57:59 +0800

    "add some other classes"

--- Entity/Goods.py
+++ Entity/Goods.py
@@ -22,5 +22,56 @@ class Good:
     def GetPrice(self):
         return self.goodNum * self.goodPrice
 
+class Food(Good):
+    taste = ''
+    shelfLife = ''
+    def __int__(self, taste, shelfLife):
+        self.taste = taste
+        self.shelfLife = shelfLife
+
+class Snack(Food):
+    weight = 0
 
 
+class Drink(Food):
+    volume = ''
+
+
+class Clothes(Good):
+    size = []
+    color = []
+    material = []
+    sex = ''
+
+
+class Shirt(Clothes):
+    collor = ''
+
+
+class Electronic(Good):
+    type = ''
+    material = ''
+    size = []
+    color = []
+
+
+class Phone(Electronic):
+    camera = ''
+    battery = ''
+    netType = ''
+
+
+class CPU:
+    frequecy = 0
+    cores = 0
+    threads = 0
+
+
+class RAM:
+    storage = 0
+    frequency = 0
+
+
+class Computer(Electronic):
+    CPU = CPU()
+    RAM = RAM()
