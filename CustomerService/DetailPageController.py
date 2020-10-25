803f25bba7afd1d2adffb60c22919d6f0f7f042c
QinXiaoshuo <52061297+QinXiaoshuo@users.noreply.github.com>
2020-10-25 17:49:35 +0800

    Update DetailPageController.py

--- CustomerService/DetailPageController.py
+++ CustomerService/DetailPageController.py
@@ -1,14 +1,18 @@
 import sys
-sys.path.append('..')
+from CustomerService.OpCart import OpCart
 from Entity.Goods import Good
 from Entity.Cart import cart
+sys.path.append('..')
+
+
 class DetailPageController:
 
-    def __init__(self,oneGood):
+    def __init__(self, oneGood):
         self.oneGood = oneGood
     
-    #将此商品添加到购物车中
-    def addGood(self,oneGood):
-        cart.AddGood(oneGood)
+    # 将此商品添加到购物车中
+    def addGood(self, oneGood, num):
+        opcart = OpCart(cart)
+        opcart.op(oneGood, num)
 
-#调整good的参数
+# 调整good的参数
