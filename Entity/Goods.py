2c678d68e2ec5079955535d92a583a00b0f39855
QinXiaoshuo <52061297+QinXiaoshuo@users.noreply.github.com>
2020-10-25 17:54:34 +0800

    Create Goods.py

---
+++ Entity/Goods.py
@@ -0,0 +1,11 @@
+import Good
+
+
+# 组合模式
+class Goods:
+    def __init__(self, good, num):
+        self.good = good
+        self.num = num
+
+    def get_price(self):
+        return num * good.goodPrice
