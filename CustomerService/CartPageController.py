e895774fef280871752e4c2addf643a7d7c4ce2b
QinXiaoshuo <52061297+QinXiaoshuo@users.noreply.github.com>
2020-10-25 17:49:07 +0800

    Update CartPageController.py

--- CustomerService/CartPageController.py
+++ CustomerService/CartPageController.py
@@ -1,22 +1,20 @@
 import sys
-sys.path.append('..')
 from Entity.Goods import Good
 from Entity.Cart import cart
+from CustomerService.OpCart import OpCart
+sys.path.append('..')
+
 
 class CartPageController:
-    def __init__(self,cart):
-        self.cart = cart
     
-    #删除购物车中的一个商品
-    def deleteGood(self,cart,index):
-        del self.cart.good[index]
+    # 增加某一个商品的数量
+    def subGoodNumber(self, index):
+        oneGood = cart.good[index]
+        opcart = OpCart(cart)
+        opcart.op(oneGood, 1)
 
-    #增加某一个商品的数量
-    def subGoodNumber(self,cart,index):
-        self.cart.good[index].goodNum -= 1
-        if self.cart.good[index].goodNum == 0:
-            self.deleteGood(cart,index)
-
-    #减少某一个商品的数量
-    def addGoodNumber(self,cart,index):
-        self.cart.good[index].goodNum += 1
+    # 减少某一个商品的数量
+    def addGoodNumber(self, index):
+        oneGood = cart.good[index]
+        opcart = OpCart(cart)
+        opcart.op(oneGood, -1)
